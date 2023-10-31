617e8e4a6e307a028a1ea6b2976a003b901622e5
Furkan Şenol <frkn.snl@hotmail.com>
2023-11-01 00:48:16 +0300

    Update Constants.kt
    API_KEY value added for the project to run

--- MovieApp/app/src/main/java/com/furkansenol/movieapp/ui/util/Constants.kt
+++ MovieApp/app/src/main/java/com/furkansenol/movieapp/ui/util/Constants.kt
@@ -2,7 +2,7 @@ package com.furkansenol.movieapp.ui.util
 
 class Constants {
     companion object {
-        const val API_KEY = null
+        const val API_KEY = "e6557bbd29e8407c944c7fe113c923e6"
         const val BASE_URL = "https://newsapi.org"
         const val SEARCH_NEWS_TIME_DELAY = 500L
         const val QUERY_PAGE_SIZE = 20
